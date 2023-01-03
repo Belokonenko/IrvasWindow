46e1f9f243596b0ff440190deb75e9d82fa03732
Belokonenko.Viacheslav@gmail <Belokonenko.Viacheslav@gmail.com>
2023-01-03 10:48:40 +0200

    Fixed bug message form post

---
+++ src/js/modules/forms.js
@@ -0,0 +1,61 @@
+const forms = () => {
+    const form = document.querySelectorAll('form');
+    const inputs = document.querySelectorAll('input');
+
+    const message = {
+        loading: 'Загрузка...',
+        success: 'Спасибо! Скоро мы с вами свяжемся.',
+        failure: 'Что-то пошло не так...',
+    };
+
+    const clearInputs = () => {
+        inputs.forEach(item => {
+            item.value = '';
+        })
+    }
+
+    const postData = async (url, data) => { // в этой функци есть асинхронные операции
+
+        console.log('test in postData ');
+        document.querySelector('.status').textContent =  message.loading;
+
+        let res = await fetch(url ,{ //ждет выполнение fetch
+            method: 'POST',
+            body: data,
+        })
+
+        return await res.text(); // ждет выполнение .text();
+
+    };
+
+    form.forEach(item => {
+        item.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            let statusMessage = document.createElement('div');
+            statusMessage.classList.add('status');
+            item.appendChild(statusMessage);
+
+            const formData = new FormData(item);
+
+            postData('assets/server.php', formData)
+                .then(res => {
+                    console.log('test postData');
+                    console.log(res);
+                    statusMessage.textContent = message.success;
+                })
+                .catch(() => {
+                    statusMessage.textContent = message.failure;
+                })
+                .finally(() => {
+                    clearInputs();
+                    setTimeout(() => { 
+                        statusMessage.remove();
+                    }, 5000)
+                })
+        })
+    })
+};
+
+export default forms;
+
